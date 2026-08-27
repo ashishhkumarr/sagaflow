3698ba45076aeef779decf3e2bae69218da55078
Ashish Kumar <ashishkumarcsee@gmail.com>
2026-08-27 21:35:47 +0530

    point order service at the outbox module

--- order-service/src/main/java/dev/ashish/order/OrderServiceApplication.java
+++ order-service/src/main/java/dev/ashish/order/OrderServiceApplication.java
@@ -1,14 +1,13 @@
 package dev.ashish.order;
 
 import dev.ashish.common.CorrelationConfig;
+import dev.ashish.outbox.OutboxConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@Import(CorrelationConfig.class)
-@EnableScheduling
+@Import({ CorrelationConfig.class, OutboxConfig.class })
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
