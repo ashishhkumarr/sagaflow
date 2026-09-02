74acc3886776975d8320b53c9e60b1651f11dc67
Ashish Kumar <ashishkumarcsee@gmail.com>
2026-09-02 20:46:32 +0530

    run the inventory tests against real kafka and postgres

--- inventory-service/src/test/java/dev/ashish/inventory/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/dev/ashish/inventory/InventoryServiceApplicationTests.java
@@ -1,10 +1,8 @@
 package dev.ashish.inventory;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class InventoryServiceApplicationTests {
+class InventoryServiceApplicationTests extends IntegrationTest {
 
 	@Test
 	void contextLoads() {
